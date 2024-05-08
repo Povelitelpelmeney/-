4d7c696d49d96a1d83e9c4c134fc19b06f096fd5
Povelitelpelmeney <120256800+Povelitelpelmeney@users.noreply.github.com>
2024-05-08 21:17:05 +0500

    Refactoring

--- vk_API/main.py
+++ vk_API/main.py
@@ -1,16 +1,30 @@
 import requests
 # Personal api key
 from config import API_KEY
+
 # Person's id vk token
-user_id = '244095300'
+USER_ID = '244095300'
+API_VERSION = "5.131"
+URL = f"https://api.vk.com/method/friends.get?user_id={USER_ID}&fields=first_name,last_name&access_token={API_KEY}&v={API_VERSION}"
 
-response = requests.get(
-    f'https://api.vk.com/method/friends.get?user_id={user_id}&fields=first_name,last_name&access_token={API_KEY}&v=5.131')
-data = response.json()
+class App:
+    def __init__(self,id,access_token,verison):
+        self.version = verison
+        self.id = id
+        self.acess_token = access_token
 
-if 'response' in data:
-    data_pars = data['response']['items']
-    for data in data_pars:
-        print(data['first_name'], data['last_name'])
-else:
-    print('Ошибка при получении списка друзей:', data)
+    @staticmethod
+    def fetch()->None:
+        response = requests.get(URL)
+        data = response.json()
+
+        if 'response' in data:
+            data_pars = data['response']['items']
+            for data in data_pars:
+                print(data['first_name'], data['last_name'])
+        else:
+            print('Ошибка при получении списка друзей:', data)
+
+
+vk_app = App(USER_ID,API_KEY,API_VERSION)
+vk_app.fetch()
